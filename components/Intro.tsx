ee34444738cc85792df3162aae808713294ce7f5
Yifei Fang <yifeifang@Yifeis-MacBook-Air.local>
2024-09-02 17:15:59 -0500

    added images to front page

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,8 +1,41 @@
-import React from "react";
-import Image from "next/image";
-import Temp from "@/public/temp.png";
+"use client";
+
+import React, { useState } from "react";
+import Image, { StaticImageData } from "next/image";
+import Pfp from "@/public/pfp.png";
+import TT1 from "@/public/tt1.png";
+import TT2 from "@/public/tt2.png";
+import TT3 from "@/public/tt3.png";
+import Food from "@/public/food.png";
+import Concert from "@/public/concert.png";
+
+const ttImages: StaticImageData[] = [TT1, TT2, TT3];
+
+const getRandomImage = (exclude: StaticImageData | null): StaticImageData => {
+  // Filter out the previously shown image
+  const filteredImages = ttImages.filter((image) => image !== exclude);
+  const randomIndex = Math.floor(Math.random() * filteredImages.length);
+  return filteredImages[randomIndex];
+};
 
 export default function Intro() {
+  const [hoveredImage, setHoveredImage] = useState<StaticImageData | null>(
+    null
+  );
+  const [previousImage, setPreviousImage] = useState<StaticImageData | null>(
+    null
+  );
+
+  const handleMouseEnterTT = () => {
+    const newImage = getRandomImage(previousImage);
+    setPreviousImage(newImage); // Update the previous image to the new one
+    setHoveredImage(newImage);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredImage(null);
+  };
+
   return (
     <div className="grid grid-cols-3 mt-28 gap-6 px-6" id="about">
       <div className="col-span-2 ml-24 flex flex-col gap-4">
@@ -19,12 +52,39 @@ export default function Intro() {
           them improve their lives with intentional design and problem-solving.
         </p>
         <p className="font-redhat text-xl font-light">
-          In my free time, I enjoy playing table tennis, going to concerts, and
-          eating tasty food.
+          In my free time, I enjoy playing{" "}
+          <span
+            onMouseEnter={handleMouseEnterTT}
+            onMouseLeave={handleMouseLeave}
+            className="cursor-pointer underline"
+          >
+            table tennis
+          </span>
+          ,{" "}
+          <span
+            onMouseEnter={() => setHoveredImage(Concert)}
+            onMouseLeave={handleMouseLeave}
+            className="cursor-pointer underline"
+          >
+            going to concerts
+          </span>
+          , and{" "}
+          <span
+            onMouseEnter={() => setHoveredImage(Food)}
+            onMouseLeave={handleMouseLeave}
+            className="cursor-pointer underline"
+          >
+            eating tasty food
+          </span>
+          .
         </p>
       </div>
       <div className="relative">
-        <Image src={Temp} alt="" className="absolute top-[-60%]" />
+        <Image
+          src={hoveredImage || Pfp}
+          alt=""
+          className="absolute top-[-60%]"
+        />
       </div>
     </div>
   );
